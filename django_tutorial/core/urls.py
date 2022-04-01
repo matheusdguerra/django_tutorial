112823cd05e528aa8abf12c5680cd0f24dfbb7b4
matheus.guerra <matheusguerra@outlook.com>
2022-03-31 23:16:51 -0300

    Views

--- django_tutorial/core/urls.py
+++ django_tutorial/core/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from django_tutorial.core.views import index
+from django_tutorial.core.views import index, detail, results, vote
 
+app_name = 'core'
 urlpatterns = [
+    # ex: /polls/
     path('', index, name='index'),
+    # ex: /polls/5/
+    path('specifics/<int:question_id>/', detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
--- django_tutorial/core/views.py
+++ django_tutorial/core/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django_tutorial.core.models import Question
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
-    return(HttpResponse("Hello, world. You're at the polls index."))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list, }
+    return render(request, 'core/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'core/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return(HttpResponse(f"You're looking at the results of question {question_id}."))
+
+
+def vote(request, question_id):
+    return(HttpResponse(f"You're voting on question {question_id}."))
