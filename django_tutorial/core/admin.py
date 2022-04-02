eb09d8806a94a8dfc646769454743cb75d491bb6
matheus.guerra <matheusguerra@outlook.com>
2022-04-02 08:00:30 -0300

    Admin Django

--- django_tutorial/core/admin.py
+++ django_tutorial/core/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 from django_tutorial.core.models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Date information', {'fields': ['pub_date']}),
+        ('Name question', {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
